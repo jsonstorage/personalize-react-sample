69094e41372e2f9574263785743e9d0e5fd4b452
jsonstorage <85914994+jsonstorage@users.noreply.github.com>
2021-06-14 22:34:20 -0400

    Add files via upload

---
+++ src/components/header.jsx
@@ -0,0 +1,41 @@
+import { usePersonalization } from "@jsonstorage/personalize-react";
+
+const config = {
+  id: "fa0f4907-6c9a-4ba4-82ac-3658ff5ed760"
+};
+
+export const Header = (props) => {
+  const variant = usePersonalization(config);
+
+  return (
+    <header id='header'>
+      <div className={'intro ' + variant.data.style}>
+        <div className='overlay'>
+          <div className='container'>
+            <div className='row'>
+              <div className='col-md-8 col-md-offset-2 intro-text'>
+                <h1>
+                  { variant.data.title }
+                  <span></span>
+                </h1>
+                <p>{ variant.data.text }</p>
+                <a
+                  href='?persona=marketer'
+                  className='btn btn-custom btn-lg page-scroll'
+                >
+                  Marketer
+                </a>{'  '}
+                <a
+                  href='?persona=developer'
+                  className='btn btn-custom btn-lg page-scroll'
+                >
+                  Developer
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
